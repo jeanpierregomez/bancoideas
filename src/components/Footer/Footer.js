bd4b8be3ccbc6582bdad145f80442d4e307a4cc8
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2021-02-19 15:44:06 -0500

    implementando footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,13 +4,21 @@ export default function Footer () {
     return (
         <footer className="f">
             <div className="f__content">
-                <img src="f__content--img"/>
+                <img class="f__content--img" src="image/logowhite.png"/>
                 <div className="f__div">
                     <ul className="f__ul">
-                        <li className="f__ul--li"><i class="fab fa-instagram"></i></li>
-                        <li className="f__ul--li"><i class="fab fa-facebook-f"></i></li>
-                        <li className="f__ul--li"><i class="fab fa-twitter"></i></li>
-                        <li className="f__ul--li"><i class="fab fa-youtube"></i></li>
+                        <a href="">
+                            <li className="f__ul--li"><i class="fab fa-instagram"></i></li>
+                        </a>
+                        <a href="">
+                            <li className="f__ul--li"><i class="fab fa-facebook-f"></i></li>
+                        </a>
+                        <a href="">
+                            <li className="f__ul--li"><i class="fab fa-twitter"></i></li>
+                        </a>
+                        <a href="">
+                            <li className="f__ul--li"><i class="fab fa-youtube"></i></li>
+                        </a>
                     </ul>
                     <a className="f__div--a" href="#">TERMINOS Y CONDICIONES</a>
                     <p className="f__div--p">COPYRIGHT © 2020 IDEAS BANK </p>

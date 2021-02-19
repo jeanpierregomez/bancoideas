1761703cb26a794fe0c6cc9f92a027e7e698f3d7
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2021-02-19 16:09:09 -0500

    implementando cursor

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
 export default function App () {
+    const handlerClick = (e) => {
+        console.log("holi")
+        let cursor = e.target;
+        cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+    }
+    const handlerMouseMove = (e) => {
+        console.log("holi")
+        let cursor = e.target;
+        cursor.classList.add("expand");
+        setTimeout(() => {
+            cursor.classList.remove("expand");
+        }, 500)
+    }
     return (
         <div className="App">
+            <div className="cursor" onClick={handlerClick} onMouseMove={handlerMouseMove}></div>
             <Header />
             <Main />
             <Footer />

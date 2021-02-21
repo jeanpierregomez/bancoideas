8948fa29f4ad36d355b7ada4579cbe310e435073
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2021-02-21 16:45:55 -0500

    vista login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,42 @@
 import FormLogin from "../../components/FormRegisterLogin/FormLogin";
+import Nav from "../../components/Layout/Nav/Nav";
+import "./index.css";
+import "./footer.css";
 
-export default function index () {
-    return (
-        <>
-            <FormLogin />
-        </>
-    );
+export default function index() {
+  return (
+    <>
+      <header className="h-login-signup"><Nav /></header>
+      <main className="main-login-signup">
+        <FormLogin />
+        <video src="/image/videologin.mp4" autoPlay="1" loop></video>
+      </main>
+      <footer className="f__login-signup">
+            <ul className="f-ls__ul">
+            <a href="">
+                <li className="f-ls__ul--li">
+                <i className="fab fa-instagram"></i>
+                </li>
+            </a>
+            <a href="">
+                <li className="f-ls__ul--li">
+                <i className="fab fa-facebook-f"></i>
+                </li>
+            </a>
+            <a href="">
+                <li className="f-ls__ul--li">
+                <i className="fab fa-twitter"></i>
+                </li>
+            </a>
+            <a href="">
+                <li className="f-ls__ul--li">
+                <i className="fab fa-youtube"></i>
+                </li>
+            </a>
+            </ul>
+            <a className="f__login-signup--a" href="#">TERMINOS Y CONDICIONES</a>
+            <p className="f__login-signup--p">COPYRIGHT © 2020 IDEAS BANK </p>
+        </footer>
+    </>
+  );
 }

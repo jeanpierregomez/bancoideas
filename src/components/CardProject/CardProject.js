5e5f73c0340a03fb5638a13d9564922387930ee1
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2021-02-22 10:47:31 -0500

    ajustes cardproject

--- src/components/CardProject/CardProject.js
+++ src/components/CardProject/CardProject.js
@@ -5,8 +5,8 @@ export default function CardProject ({ project }) {
         <figure className="s-fp__fig">
             <img className="s-fp__fig--img" src={project.image} />
             <figcaption className="s-fp__figcaption">
+                <p className="">Enero 2, 2021</p>
                 <h2 className="s-fp__figcaption--h2">{project.title}</h2>
-                <p className="s-fp__figcaption--p">{project.description}</p>
             </figcaption>
         </figure>
     );
